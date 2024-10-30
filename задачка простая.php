0d683d4b17fc518e7fef8a2a82ca2dc59fe969a6
alimaga888 <159657665+alimaga888@users.noreply.github.com>
2024-10-30 18:06:29 +0300

    простая_задачка

---
+++ задачка простая.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operation = $_POST['operation'];
+    
+    switch ($operation) {
+        case 'add':
+            $result = $num1 + $num2;
+            break;
+        case 'subtract':
+            $result = $num1 - $num2;
+            break;
+        case 'multiply':
+            $result = $num1 * $num2;
+            break;
+        case 'divide':
+            $result = $num2 != 0 ? $num1 / $num2 : 'Ошибка: деление на ноль';
+            break;
+        default:
+            $result = 'Некорректная операция';
+    }
+}
+?>
